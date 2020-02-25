f2f1dbeb2867aa077845a1d92af5e05f545c8f89
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-02-25 15:46:23 -0700

    working

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js", { scope: "/" })
+    .then(() => console.log("Service Worker registered successfully."))
+    .catch(error => console.log("Service Worker registration failed:", error));
+}
+
 let transactions = [];
 let myChart;
 
